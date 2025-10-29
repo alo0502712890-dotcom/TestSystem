d233975126a1a400f1230e094a6df298a8e05fdc
Алена Бенедюк <alo0502712890@gmail.com>
2025-10-29 03:59:36 +0200

    додали сторінку студента

--- Data/TestSystemContext.cs
+++ Data/TestSystemContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,7 +29,8 @@ namespace TestSystem.Data
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("Server=Beneduk\\SQLEXPRESS;Database=TestSystem;Trusted_Connection=True;TrustServerCertificate=True;");
+                string connectionString = ConfigurationManager.ConnectionStrings["SQLProvider"].ConnectionString;
+                optionsBuilder.UseSqlServer(connectionString);
             }
         }
 
---
+++ Models/TestSessionInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSystem.Models
+{
+    public class TestSessionInfo
+    {
+        public int SessionID { get; set; }
+        public string TestName { get; set; } // <--- Це те, чого не вистачає в TestSession
+        public DateTime StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public decimal? Score { get; set; }
+        public decimal? MaxScore { get; set; }
+        public bool IsCompleted { get; set; }
+
+
+        // Допоміжні властивості для відображення
+        public string CompletionDate => EndTime.HasValue ? EndTime.Value.ToString("dd.MM.yyyy HH:mm") : "Триває";
+        public string DisplayScore => IsCompleted ? $"{Score}/{MaxScore}" : "N/A";
+    }
+}
